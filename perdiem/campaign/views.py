aa04f10dedd0c6a7c17c9079727d8548652253d3
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-06-12 22:00:00 -0700

    Updated leaderboard context to include top artists

--- perdiem/campaign/views.py
+++ perdiem/campaign/views.py
@@ -5,25 +5,55 @@
 """
 
 from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.views.generic import TemplateView
 
-from accounts.models import UserProfile
+from accounts.models import UserAvatar, UserProfile
+from artist.models import Artist, Photo
 
 
 class LeaderboardView(TemplateView):
 
     template_name = 'leaderboard/leaderboard.html'
 
+    def investor_context(self, investor):
+        context = investor.profile_context()
+        context.update({
+            'name': investor.get_display_name(),
+            'url': investor.public_profile_url(),
+            'avatar_url': investor.avatar_url(),
+        })
+        return context
+
+    def artist_context(self, artist):
+        try:
+            avatar_url = artist.photo.img.url
+        except Photo.DoesNotExist:
+            avatar_url = UserAvatar.default_avatar_url()
+        return {
+            'name': artist.name,
+            'url': reverse('artist', kwargs={'slug': artist.slug,}),
+            'avatar_url': avatar_url,
+            'total_earned': artist.total_earned,
+        }
+
     # TODO(lucas): Review to improve performance
-    # Warning: This absolutely will not scale, the view is meant to be run
-    # occasionally (once a day) and then have the whole page cached
+    # Warning: total_earned absolutely will not scale, the view is meant to
+    # be run occasionally (once a day) and then have the whole page cached
     def calculate_leaderboard(self):
-        all_users = [(userp, userp.profile_context()) for userp in UserProfile.objects.filter(invest_anonymously=False)]
-        total_earned = filter(lambda (userp, pcontext): pcontext['total_earned'] > 0, all_users)
-        total_earned = sorted(total_earned, key=lambda (userp, pcontext): pcontext['total_earned'], reverse=True)[:5]
+        # Investor total earned
+        user_profiles = UserProfile.objects.filter(invest_anonymously=False)
+        total_earned = [self.investor_context(user_profile) for user_profile in user_profiles]
+        total_earned = filter(lambda context: context['total_earned'] > 0, total_earned)
+        total_earned = sorted(total_earned, key=lambda context: context['total_earned'], reverse=True)[:5]
+
+        # Artist total earned
+        artists = Artist.objects.all().annotate(total_earned=models.Sum('campaign__revenuereport__amount')).filter(total_earned__isnull=False).order_by('-total_earned')[:5]
+        revenue_generated = [self.artist_context(artist) for artist in artists]
         return {
             'total_earned': total_earned,
+            'revenue_generated': revenue_generated,
         }
 
     def get_context_data(self, **kwargs):
