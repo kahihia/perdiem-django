6705aa31a41b2a919194c24321968c7417b4119f
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-07-16 12:28:44 -0700

    Added Top Raised and Top Invested to leaderboard page

--- perdiem/campaign/views.py
+++ perdiem/campaign/views.py
@@ -40,19 +40,44 @@ class LeaderboardView(TemplateView):
         }
 
     # TODO(lucas): Review to improve performance
-    # Warning: top_earned_investors absolutely will not scale, the view is meant to
-    # be run occasionally (once a day) and then have the whole page cached
+    # Warning: top_invested and top_earned_investors absolutely will not scale,
+    # the view is meant to be run occasionally (once a day) and then have the
+    # whole page cached
     def calculate_leaderboard(self):
-        # Top earned investors
+        # Top raised
+        all_artists = Artist.objects.all()
+        top_raised = all_artists.annotate(
+            amount=models.Sum(
+                models.Case(
+                    models.When(
+                        project__campaign__investment__charge__paid=True,
+                        then=models.F('project__campaign__investment__num_shares') * models.F('project__campaign__value_per_share')
+                    ),
+                    default=0,
+                    output_field=models.IntegerField()
+                )
+            )
+        ).filter(amount__gt=0).order_by('-amount')[:5]
+        top_raised = [self.artist_context(artist) for artist in top_raised]
+
+        # Top invested
         user_profiles = UserProfile.objects.filter(invest_anonymously=False)
+        top_invested = [self.investor_context(user_profile, 'total_investments') for user_profile in user_profiles]
+        top_invested = filter(lambda context: context['amount'] > 0, top_invested)
+        top_invested = sorted(top_invested, key=lambda context: context['amount'], reverse=True)[:5]
+
+        # Top earned investors
         top_earned_investors = [self.investor_context(user_profile, 'total_earned') for user_profile in user_profiles]
         top_earned_investors = filter(lambda context: context['amount'] > 0, top_earned_investors)
         top_earned_investors = sorted(top_earned_investors, key=lambda context: context['amount'], reverse=True)[:5]
 
         # Top earned artists
-        artists = Artist.objects.all().annotate(amount=models.Sum('project__revenuereport__amount')).filter(amount__isnull=False).order_by('-amount')[:5]
-        top_earned_artists = [self.artist_context(artist) for artist in artists]
+        top_earned_artists = all_artists.annotate(amount=models.Sum('project__revenuereport__amount')).filter(amount__isnull=False).order_by('-amount')[:5]
+        top_earned_artists = [self.artist_context(artist) for artist in top_earned_artists]
+
         return {
+            'top_raised': top_raised,
+            'top_invested': top_invested,
             'top_earned_artists': top_earned_artists,
             'top_earned_investors': top_earned_investors,
         }
