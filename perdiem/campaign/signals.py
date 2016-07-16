12887aa8a3acc7c05f940a8758dd6e13faced0fe
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-07-16 12:34:05 -0700

    Clear cache when an investment is made

--- perdiem/campaign/signals.py
+++ perdiem/campaign/signals.py
@@ -8,9 +8,10 @@ from django.core.cache import cache
 from django.db import models
 from django.dispatch import receiver
 
-from campaign.models import RevenueReport
+from campaign.models import Investment, RevenueReport
 
 
-@receiver(models.signals.post_save, sender=RevenueReport, dispatch_uid="clear_leaderboard_cache_handler")
+@receiver(models.signals.post_save, sender=Investment, dispatch_uid="clear_leaderboard_cache_from_investment_handler")
+@receiver(models.signals.post_save, sender=RevenueReport, dispatch_uid="clear_leaderboard_cache_from_revenue_report_handler")
 def clear_leaderboard_cache_handler(sender, instance, **kwargs):
     cache.delete('leaderboard')
