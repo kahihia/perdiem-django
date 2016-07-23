650cdfd81b99b04d1d9dc07c4b22bb269c00bfeb
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-07-23 15:47:54 -0700

    Mock Geocoder in tests for Coordinates API

--- perdiem/api/tests.py
+++ perdiem/api/tests.py
@@ -6,8 +6,9 @@
 
 import mock
 
-from perdiem.tests import PerDiemTestCase
+from geopy.exc import GeocoderTimedOut
 
+from perdiem.tests import PerDiemTestCase
 from artist.models import Update
 
 
@@ -16,11 +17,19 @@ class CoordinatesFromAddressTestCase(PerDiemTestCase):
     url = '/api/coordinates/?address={address}'
     valid_url = url.format(address='Willowdale%2C+Toronto%2C+Ontario%2C+Canada')
 
-    def testCoordinatesFromAddress(self):
+    @mock.patch('api.views.Nominatim.geocode')
+    def testCoordinatesFromAddress(self, mock_geocode):
+        # First the Geocoder service fails and so we return 503
+        mock_geocode.side_effect = GeocoderTimedOut
+        self.assertAPIResponseRenders(self.valid_url, status_code=503)
+
+        # Then the Geocoder service kicks back online and we succeed
+        mock_geocode.side_effect = None
+        mock_geocode.return_value = mock.Mock(latitude=43.766751, longitude=-79.410332)
         response = self.assertAPIResponseRenders(self.valid_url)
         lat, lon = response['latitude'], response['longitude']
-        self.assertAlmostEquals(lat, 43.7689, places=2)
-        self.assertAlmostEquals(lon, -79.4138, places=2)
+        self.assertEquals(lat, 43.7668)
+        self.assertEquals(lon, -79.4103)
 
     def testCoordinatesFromAddressRequiresAddress(self):
         for url in ['/api/coordinates/', self.url.format(address=''),]:
