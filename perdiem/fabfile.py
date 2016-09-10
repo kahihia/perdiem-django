fc90c83c088d7b3b5d941382e0281a457e3155b9
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-09-10 15:24:49 -0700

    Include commit information in deploy notification

--- perdiem/fabfile.py
+++ perdiem/fabfile.py
@@ -13,7 +13,7 @@ import requests
 env.hosts = os.environ.get('PERDIEM_REMOTE_HOSTS', '').split(',')
 
 
-def send_notification():
+def send_notification(commit):
     bot_token = os.environ.get('PERDIEM_DEPLOYBOT_TOKEN')
     if not bot_token:
         return
@@ -21,7 +21,7 @@ def send_notification():
     data = {
         'token': bot_token,
         'channel': '#general',
-        'text': 'Deploy complete',
+        'text': '`{commit}`\nhas been deployed'.format(commit=commit),
         'as_user': True,
     }
     requests.post('https://slack.com/api/chat.postMessage', data=data)
@@ -30,8 +30,9 @@ def send_notification():
 def deploy():
     with prefix(". /usr/local/bin/virtualenvwrapper.sh; workon perdiem"):
         run("git pull")
+        latest_commit = run("git log -1 --format=\"%h | %an | %s\" --no-color", pty=False)
         run("pip install -r ../requirements.txt")
         run("python manage.py migrate")
         run("python manage.py collectstatic --no-input")
         sudo("sv restart perdiem")
-    send_notification()
+    send_notification(latest_commit)
