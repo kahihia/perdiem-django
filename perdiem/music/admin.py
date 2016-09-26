b25f1c1a8055cb643670f20917b147360ed2397a
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-09-25 17:18:43 -0700

    Verify that an album has campaigns before creating activity estimates

--- perdiem/music/admin.py
+++ perdiem/music/admin.py
@@ -12,6 +12,36 @@ from pagedown.widgets import AdminPagedownWidget
 from music.models import Album, Track, Artwork, AlbumBio, MarketplaceURL, Audio, ActivityEstimate
 
 
+class ActivityEstimateAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = ActivityEstimate
+        fields = ('date', 'activity_type', 'content_type', 'object_id', 'total',)
+
+    def clean(self):
+        cleaned_data = super(ActivityEstimateAdminForm, self).clean()
+
+        # Get the object associated with this ActivityEstimate
+        content_type = cleaned_data['content_type']
+        object_id = cleaned_data['object_id']
+        obj = content_type.get_object_for_this_type(id=object_id)
+
+        # Get the album associated with this ActivityEstimate
+        if hasattr(obj, 'album'):
+            album = obj.album
+        else:
+            album = obj
+
+        # Verify that the associated album has a campaign defined
+        if not album.project.campaign_set.all().exists():
+            raise forms.ValidationError(
+                "You cannot create activity estimates without defining the revenue percentages "
+                "issued to artists and fans. You must first create a campaign."
+            )
+
+        return cleaned_data
+
+
 class TrackInline(admin.StackedInline):
 
     model = Track
@@ -55,6 +85,11 @@ class AlbumAdmin(admin.ModelAdmin):
     inlines = (TrackInline, ArtworkInline, AlbumBioInline, MarketplaceURLInline, AudioInline,)
 
 
+class ActivityEstimateAdmin(admin.ModelAdmin):
+
+    form = ActivityEstimateAdminForm
+
+
 admin.site.register(Album, AlbumAdmin)
 admin.site.register(Track)
-admin.site.register(ActivityEstimate)
+admin.site.register(ActivityEstimate, ActivityEstimateAdmin)
