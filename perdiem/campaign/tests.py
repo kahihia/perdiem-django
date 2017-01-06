1ec0ecf1ade14a199af4f5aadcd906c294fd19e7
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-05 20:53:22 -0500

    Add test to handle case where campaign end datetime
    comes before start datetime

--- perdiem/campaign/tests.py
+++ perdiem/campaign/tests.py
@@ -4,6 +4,8 @@
 
 """
 
+import datetime
+
 from perdiem.tests import PerDiemTestCase
 
 
@@ -30,6 +32,31 @@ class CampaignAdminWebTestCase(PerDiemTestCase):
         self.campaign.amount = 0
         self.campaign.save()
         self.assertEquals(self.campaign.percentage_funded(), 100)
+
+    def testCampaignEndCannotComeBeforeStart(self):
+        start_datetime = datetime.datetime(year=2017, month=2, day=1)
+        end_datetime = datetime.datetime(year=2017, month=1, day=1)
+
+        data = {
+            'project': self.project.id,
+            'amount': self.CAMPAIGN_AMOUNT,
+            'value_per_share': 1,
+            'start_datetime_0': start_datetime.strftime('%Y-%m-%d'),
+            'start_datetime_1': start_datetime.strftime('%H:%M:%S'),
+            'end_datetime_0': end_datetime.strftime('%Y-%m-%d'),
+            'end_datetime_1': end_datetime.strftime('%H:%M:%S'),
+            'use_of_funds': '',
+            'fans_percentage': 50,
+            'expense_set-TOTAL_FORMS': 0,
+            'expense_set-INITIAL_FORMS': 0,
+        }
+        response = self.assertResponseRenders(
+            '/admin/campaign/campaign/add/',
+            method='POST',
+            data=data,
+            has_form_error=True
+        )
+        self.assertIn("Campaign cannot end before it begins.", response.content)
 
 
 class CampaignWebTestCase(PerDiemTestCase):
