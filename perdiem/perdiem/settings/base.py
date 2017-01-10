1b917feb0b0c66f0343c20fae2e90b42f6462621
Brandon Nelson <brandonnelson@me.com>
2017-01-10 03:23:56 -0800

    Updated default min purchase from 10 to 1

--- perdiem/perdiem/settings/base.py
+++ perdiem/perdiem/settings/base.py
@@ -239,7 +239,7 @@ class BaseSettings(DjangoDefaults):
     PERDIEM_FEE = 1  # $1
     STRIPE_PERCENTAGE = 0.029  # 2.9%
     STRIPE_FLAT_FEE = 0.3  # $0.30
-    DEFAULT_MIN_PURCHASE = 10  # $10
+    DEFAULT_MIN_PURCHASE = 1  # $1
     PINAX_STRIPE_SEND_EMAIL_RECEIPTS = False
 
     # Analytics
