28e381dacd7c334536373b2098c3756e405f2f87
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-14 15:30:42 -0800

    Add percentage to user profile context

--- perdiem/accounts/models.py
+++ perdiem/accounts/models.py
@@ -159,4 +159,10 @@ class UserProfile(models.Model):
             total_earned += generated_revenue_user
         context['total_earned'] = total_earned
 
+        try:
+            percentage = total_earned / aggregate_context['total_investments'] * 100
+        except ZeroDivisionError:
+            percentage = 0
+        context['percentage'] = percentage
+
         return context
