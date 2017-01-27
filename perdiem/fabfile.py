1e8a3880825fd3192a3a984de91a8e93b5892bda
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-26 20:54:04 -0800

    Post all changes to Slack, rather than just the most recent commit

--- perdiem/fabfile.py
+++ perdiem/fabfile.py
@@ -13,7 +13,7 @@ import requests
 env.hosts = os.environ.get('PERDIEM_REMOTE_HOSTS', '').split(',')
 
 
-def send_notification(commit):
+def send_notification(commits):
     bot_token = os.environ.get('PERDIEM_DEPLOYBOT_TOKEN')
     if not bot_token:
         return
@@ -21,7 +21,7 @@ def send_notification(commit):
     data = {
         'token': bot_token,
         'channel': '#general',
-        'text': '`{commit}`\nhas been deployed'.format(commit=commit),
+        'text': '`{commits}`\nhas been deployed'.format(commits=commits),
         'as_user': True,
     }
     requests.post('https://slack.com/api/chat.postMessage', data=data)
@@ -29,10 +29,14 @@ def send_notification(commit):
 
 def deploy():
     with prefix(". /usr/local/bin/virtualenvwrapper.sh; workon perdiem"):
+        previous_commit_hash = run("git log -1 --format=\"%H\" --no-color", pty=False)
         run("git pull")
-        latest_commit = run("git log -1 --format=\"%h : %an : %s\" --no-color", pty=False)
+        cmd_changes_deployed = "git log {previous_hash}.. --format=\"%h : %an : %s\" --no-color".format(
+            previous_hash=previous_commit_hash
+        )
+        changes_deployed = run(cmd_changes_deployed, pty=False)
         run("pip install -r ../requirements.txt")
         run("python manage.py migrate")
         run("python manage.py collectstatic --no-input")
         sudo("sv restart perdiem")
-    send_notification(latest_commit)
+    send_notification(changes_deployed)
