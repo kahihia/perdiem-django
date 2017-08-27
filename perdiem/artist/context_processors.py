1f9ed78d7eee47d74165fdebdf14c032fd48a356
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-08-27 16:57:44 -0700

    Use PERDIEM_FEE from settings instead of hardcoding 1 dollar
    in the template for the home page

---
+++ perdiem/artist/context_processors.py
@@ -0,0 +1,13 @@
+"""
+:Created: 27 August 2017
+:Author: Lucas Connors
+
+"""
+
+from django.conf import settings
+
+
+def artist_settings(request):
+    return {
+        'PERDIEM_FEE': settings.PERDIEM_FEE,
+    }
