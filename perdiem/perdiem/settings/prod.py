e301188c63cd7e9dd9a3f7acbdf26684d7b04b91
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-08-03 19:56:33 -0700

    FIX: Define settings as properties for code that accesses it directly

--- perdiem/perdiem/settings/prod.py
+++ perdiem/perdiem/settings/prod.py
@@ -10,6 +10,7 @@ class ProdSettings(BaseSettings):
     DEBUG = False
 
     # Sentry
+    @property
     def RAVEN_CONFIG(self):
         return {
             'dsn': 'https://{public_key}:{secret_key}@app.getsentry.com/{project_id}'.format(
@@ -24,23 +25,29 @@ class ProdSettings(BaseSettings):
     DEFAULT_FILE_STORAGE = 'django_s3_storage.storage.S3Storage'
     STATICFILES_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
 
+    @property
     def AWS_S3_BUCKET_NAME(self):
         return os.environ["PERDIEM_AWS_S3_BUCKET_NAME"]
 
+    @property
     def AWS_S3_BUCKET_NAME_STATIC(self):
         return self.AWS_S3_BUCKET_NAME
 
+    @property
     def AWS_ACCESS_KEY_ID(self):
         return os.environ["PERDIEM_AWS_ACCESS_KEY_ID"]
 
+    @property
     def AWS_SECRET_ACCESS_KEY(self):
         return os.environ["PERDIEM_AWS_SECRET_ACCESS_KEY"]
 
     # Email
     EMAIL_BACKEND = 'django_ses.SESBackend'
 
+    @property
     def AWS_SES_ACCESS_KEY_ID(self):
         return os.environ["PERDIEM_AWS_SES_ACCESS_KEY_ID"]
 
+    @property
     def AWS_SES_SECRET_ACCESS_KEY(self):
         return os.environ["PERDIEM_AWS_SES_SECRET_ACCESS_KEY"]
