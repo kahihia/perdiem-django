80a8375583bd9747eb780b3a800024be1b4989fb
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-09-08 14:57:58 -0700

    Post to Slack when failure occurs during deployment (#332)
    * Split up deploy into three steps:
    
    1) _pull_latest_changes() (ie. get latest code from origin)
    2) _perform_update() (ie. update dependencies, run migrations, etc.)
    3) _restart() (ie. restart runit)
    
    * Restart nginx when deploying
    
    Also expose restart step to fabric CLI
    
    * Use git rev-parse instead of git log -1 --format="%H"
    
    * Post to Slack when failure occurs during deployment

--- fabfile.py
+++ fabfile.py
@@ -6,38 +6,79 @@
 
 import os
 
-from fabric.api import env, prefix, run, sudo
-from fabric.context_managers import cd
 import requests
+from fabric.api import env, run, sudo
+from fabric.context_managers import cd
 
+PROJECT_DIR = "~/perdiem-django"
 
 env.hosts = os.environ.get("PERDIEM_REMOTE_HOSTS", "").split(",")
 
 
-def send_notification(commits):
+def _pull_latest_changes():
+    """
+    Pull latest code from origin
+    :return: Description of the changes since the last deploy
+    """
+    with cd(PROJECT_DIR):
+        previous_commit_hash = run("git rev-parse HEAD", pty=False)
+        run("git pull")
+        cmd_changes_pulled = f'git log {previous_commit_hash}.. --reverse --format="%h : %an : %s" --no-color'
+        changes_pulled = run(cmd_changes_pulled, pty=False)
+    return changes_pulled
+
+
+def _perform_update():
+    """
+    Update dependencies, run migrations, etc.
+    """
+    with cd(PROJECT_DIR):
+        run("poetry install --no-dev")
+        run("poetry run python manage.py migrate")
+        run("poetry run python manage.py collectstatic --no-input")
+
+
+def _send_notification(commits, deploy_successful):
+    """
+    Post update to Slack
+    """
     bot_token = os.environ.get("PERDIEM_DEPLOYBOT_TOKEN")
     if not bot_token:
         return
 
-    text = (
-        f"```\n{commits}\n```\nhas been deployed"
-        if commits
-        else "Services were restarted"
-    )
+    if commits:
+        if deploy_successful:
+            deploy_status = "completed successfully"
+        else:
+            deploy_status = "started, but was not completed successfully"
+        text = f"Deploy {deploy_status}. Changelog:\n```\n{commits}\n```"
+    elif deploy_successful:
+        text = "Services were restarted successfully."
+    else:
+        text = "Attempted to restart services, but a failure occurred."
+
     data = {"token": bot_token, "channel": "#general", "text": text, "as_user": True}
     requests.post("https://slack.com/api/chat.postMessage", data=data)
 
 
+def restart():
+    """
+    Restart services
+    """
+    sudo("sv restart perdiem")
+    sudo("service nginx restart")
+
+
 def deploy():
-    with cd("~/perdiem-django"):
-        previous_commit_hash = run('git log -1 --format="%H" --no-color', pty=False)
-        run("git pull")
-        cmd_changes_deployed = 'git log {previous_hash}.. --reverse --format="%h : %an : %s" --no-color'.format(
-            previous_hash=previous_commit_hash
-        )
-        changes_deployed = run(cmd_changes_deployed, pty=False)
-        run("poetry install --no-dev")
-        run("poetry run python manage.py migrate")
-        run("poetry run python manage.py collectstatic --no-input")
-        sudo("sv restart perdiem")
-    send_notification(changes_deployed)
+    """
+    Perform update, restart services, and send notification to Slack
+    """
+    changes_pulled = _pull_latest_changes()
+
+    deploy_successful = False
+    try:
+        _perform_update()
+        restart()
+        deploy_successful = True
+    finally:
+        _send_notification(changes_pulled, deploy_successful)
